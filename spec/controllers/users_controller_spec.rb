18bc6cb30efa605044a5a512d3309dc4715e98d1
Itamar Hassin <ihassin@mac.com>
2015-03-26 11:31:49 -0400

    Added User controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  before(:each) do
+  end
+
+  def iphone_data
+    {
+        :latitude => 1,
+        :longitude => 2,
+        :vendor_id => 'abc-def',
+        :tag_id => 3
+    }
+  end
+
+  describe "PUT create", :type => :controller do
+    it "returns proper json to a user create request" do
+      post :create, :user => iphone_data, :format => :json
+
+      resp = JSON.parse(response.body)
+      expect(resp['id']).to_not eq(0)
+      expect(resp['latitude']).to eq(1)
+      expect(resp['longitude']).to eq(2)
+      expect(resp['vendor_id']).to eq('abc-def')
+      expect(resp['tag_id']).to eq(3)
+    end
+  end
+
+  describe "PUT update" do
+    it "updates user info correctly" do
+      user = User.create! iphone_data
+
+      put :update, :id => user.id, :user => iphone_data.merge(:latitude => 90), :format => :json
+
+      expect(response.status).to eq 200
+      user.reload
+      expect(user.latitude).to eq(90)
+    end
+
+  end
+
+  describe "GET show", :type => :controller do
+    it "returns proper json to a user get request" do
+      user = User.create! iphone_data
+
+      get :show, :id => user.id, :format => :json
+
+      resp = JSON.parse(response.body)
+      expect(resp['latitude']).to eq 1
+    end
+
+  end
+end
