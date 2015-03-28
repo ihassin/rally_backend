e3eef6ea5b52059f19e310147c0cc8dc7ac8b954
Itamar Hassin <ihassin@mac.com>
2015-03-28 10:37:47 -0400

    Added unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deploy/rallyserver/current"
+pid "/home/deploy/rallyserver/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/rallyserver/current/log/unicorn.log"
+stdout_path "/home/deploy/rallyserver/current/log/unicorn.log"
+
+listen "/tmp/unicorn.rallyserver.sock"
+worker_processes 2
+timeout 30
